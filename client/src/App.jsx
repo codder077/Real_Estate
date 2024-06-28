aaab5d7a499431790e5a5a16c301723f92ad5fa1
codder077 <120477792+codder077@users.noreply.github.com>
2024-06-28 12:20:45 +0530

    search functionality added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing  from './pages/UpdateListing';
 import Listing from './pages/Listing'
+import Search from './pages/Search'
 
 function App() {
 
@@ -22,6 +23,7 @@ function App() {
       <Route path='/about' element={<About/>}/>
       <Route path='/sign-in' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/search' element={<Search />} />
       <Route path='/listing/:listingId' element={<Listing />} />
       <Route element={<PrivateRoute/>}>
         <Route path='/profile' element={<Profile/>}/>
